ff70a2e2c1921be9a2a6798f9be3d750a5a5708f
Rohit Mendhekar <103194502+rohit99878@users.noreply.github.com>
2023-07-28 22:35:39 +0530

    Add files via upload

---
+++ HW.java
@@ -0,0 +1,14 @@
+class A{
+	
+	public static void main(String[] args){
+		System.out.print("Hello World!!!");
+	}
+}
+
+class B{
+	
+	public static void main(String[] args){
+		System.out.print("Hello world-BX!!!");
+		
+	}
+}
